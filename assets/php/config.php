02a21c11f260eaf956180ede1604e69bc4f42652
japped <64382915+japped@users.noreply.github.com>
2020-11-22 02:57:04 +0200

    Update config.php

--- assets/php/config.php
+++ assets/php/config.php
@@ -1,8 +1,8 @@
 <?php
-define("DB_HOST", "127.0.0.1");
-define("DB_USER", "bothosting");
-define("DB_PASSWORD", "PASSWORD");
-define("DB_DATABASE", "DATABASE");
+define("DB_HOST", "sql7.freemysqlhosting.net");
+define("DB_USER", "sql7377964");
+define("DB_PASSWORD", "P5tpQ5XuJv");
+define("DB_DATABASE", "sql7377964");
 define("DB_PREFIX", "kresu24_");
 
 $SQL = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
@@ -10,9 +10,9 @@ global $DB;
 $DB = $SQL;
 
 define("SSH_PORT", 22);
-define("SSH_HOST", "ADDRESS");
-define("SSH_USER", "root");
-define("SSH_PASSWORD", "PASSWORD");
+define("SSH_HOST", "usa.lionssh.xyz");
+define("SSH_USER", "Jappe-lionssh.com");
+define("SSH_PASSWORD", "0415668017");
 
 define("DIR_HOME", "/home/bots/");
 define("DIR_USER", DIR_HOME . "user/");
